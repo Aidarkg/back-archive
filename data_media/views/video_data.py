846f640197ce0e076742b60205c47d63a4743069
Aidarkg <adijamgyrchiev@gmail.com>
2024-04-28 17:20:08 +0600

    Добавил поиск и пагинацию

--- data_media/views/video_data.py
+++ data_media/views/video_data.py
@@ -1,22 +1,14 @@
-from rest_framework.views import APIView
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import RetrieveAPIView, ListAPIView
+from rest_framework.pagination import PageNumberPagination
 from data_media.serializers.video_data import VideoDataSerializer
 from data_media.models import VideoData
-from rest_framework.response import Response
-from rest_framework import status
 
 
-class VideoDataAPIView(APIView):
-    def get(self, request):
-        video_data = VideoData.objects.all()
-        serializer = VideoDataSerializer(video_data, many=True).data
-        return Response(serializer)
-    def post(self, request):
-        serializer = VideoDataSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class VideoDataAPIView(ListAPIView):
+    queryset = VideoData.objects.all()
+    serializer_class = VideoDataSerializer
+    pagination_class = PageNumberPagination
+    filterset_fields = ['title']
     
 
 class VideoDataRetriveAPIView(RetrieveAPIView):
