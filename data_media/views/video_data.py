f86ef230bcce20c2a031c161052b57a9fbd5dea7
Aidarkg <adijamgyrchiev@gmail.com>
2024-05-02 15:13:29 +0600

    Улучшил поиск и сделал сортировку

--- data_media/views/video_data.py
+++ data_media/views/video_data.py
@@ -1,14 +1,26 @@
 from rest_framework.generics import RetrieveAPIView, ListAPIView
 from rest_framework.pagination import PageNumberPagination
+from django.db.models import Q
 from data_media.serializers.video_data import VideoDataSerializer
 from data_media.models import VideoData
 
 
 class VideoDataAPIView(ListAPIView):
-    queryset = VideoData.objects.all()
+    queryset = VideoData.objects.all().order_by('-created_at')
     serializer_class = VideoDataSerializer
     pagination_class = PageNumberPagination
-    filterset_fields = ['title']
+
+    def get_queryset(self):
+        queryset = VideoData.objects.all()
+
+        search_query = self.request.query_params.get('search', None)
+        
+        if search_query:
+            queryset = queryset.filter(
+                Q(title__icontains=search_query)
+            )
+        
+        return queryset
     
 
 class VideoDataRetriveAPIView(RetrieveAPIView):
