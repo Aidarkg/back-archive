c09fa47825bd1f208a3cbc4e0698698b910e6eb9
Nurikov <asankylovnursultan@gmail.com>
2024-05-04 23:37:42 +0600

    Create translation for models in data_media

---
+++ data_media/translation.py
@@ -0,0 +1,32 @@
+from modeltranslation.translator import register, TranslationOptions
+from .models import kodeks, management, news, photo_gallery_models, service, video_data
+
+
+@register(kodeks.KODEKS)
+class KodeksTranslationOptions(TranslationOptions):
+    fields = ('title',)
+
+
+@register(management.Management)
+class ManagementTranslationOptions(TranslationOptions):
+    fields = ('full_name', 'position', '')
+
+
+@register(news.News)
+class NewsTranslationOptions(TranslationOptions):
+    fields = ('title', 'description', 'detailed_description')
+
+
+@register(photo_gallery_models.PhotoGallery)
+class PhotoTranslationOptions(TranslationOptions):
+    fields = ('title', 'description', '')
+
+
+@register(service.Service)
+class ServiceTranslationOptions(TranslationOptions):
+    fields = ('title', 'content', 'status', )
+
+
+@register(video_data.VideoData)
+class PhotoTranslationOptions(TranslationOptions):
+    fields = ('title', 'description', )
