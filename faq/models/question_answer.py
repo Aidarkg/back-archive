72fa308cb918f65337e0374ca3a750a3431d7b69
Nurikov <asankylovnursultan@gmail.com>
2024-05-04 23:06:51 +0600

    Отрефакторил модель вопрос-ответ, и переписал view на generic

--- faq/models/question_answer.py
+++ faq/models/question_answer.py
@@ -1,14 +1,10 @@
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-
+from common.models.mixins import DateTimeMixin
 from faq.validators import validate_phone_number
 from moderator.models import Moderator
-from django.conf import settings
 
 
-class Question(models.Model):
+class Question(DateTimeMixin):
     full_name = models.CharField(
         max_length=150,
         null=False,
@@ -28,26 +24,18 @@ class Question(models.Model):
         verbose_name='Номер телефона',
         validators=[validate_phone_number]
     )
-    topic_question = models.CharField(
-        max_length=100,
-        null=True,
-        blank=True,
-        verbose_name='Тема вопроса'
-    )
     question_text = models.CharField(
         max_length=500,
         null=False,
         blank=False,
         verbose_name='Вопрос'
     )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.topic_question # Надо обсудить
+        return self.question_text
 
 
-class Answer(models.Model):
+class Answer(DateTimeMixin):
     moderator = models.ForeignKey(
         Moderator,
         on_delete=models.DO_NOTHING,
@@ -59,19 +47,3 @@ class Answer(models.Model):
         related_name='question_answer'
     )
     answer = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
-@receiver(post_save, sender=Answer)
-def send_mail_to_user(sender, instance, **kwargs):
-    from faq.tasks import send_mail
-
-    question = instance.question
-    answer = instance.answer
-    send_mail.delay(
-        question.id,
-        from_email=settings.EMAIL_HOST_USER,
-        recipient_list=[question.email],
-        answer=answer
-    )
