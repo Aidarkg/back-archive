ef05e7710f8731da08b59afcff69f69b353afda2
Nurikov <asankylovnursultan@gmail.com>
2024-05-04 23:05:26 +0600

    Отрефакторил модель faq переписал view

--- faq/apps.py
+++ faq/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class FaqConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'faq'
+    verbose_name = 'Вопросы-Ответы'
+    verbose_name_plural = 'Вопросы-Ответы'
