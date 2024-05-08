5f1d24a4b450487a9db0a73ddbb670191a85c640
Nurikov <asankylovnursultan@gmail.com>
2024-05-08 19:44:27 +0600

    Finally configed translation

--- archive/urls.py
+++ archive/urls.py
@@ -7,13 +7,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('data_media.urls')),
-    path('', include('faq.urls')),
-    path('i18n/', include('django.conf.urls.i18n')),
 ]
 
-# urlpatterns += i18n_patterns(
-#     path('', include('faq.urls')),
-# )
+urlpatterns += i18n_patterns(
+    path('', include('data_media.urls')),
+    path('', include('faq.urls')),
+)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- common/admin/mixins.py
+++ common/admin/mixins.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
 
 
-class BaseAdminMixin(admin.ModelAdmin):
+class BaseAdminMixin(TranslationAdmin):
     search_fields = 'title',
     list_filter = ('created_at', 'updated_at')
     list_display_links = ('id', 'title')
 
     readonly_fields = ('id', 'created_at', 'updated_at')
+    group_fieldsets = True
+
+    class Media:
+        js = (
+            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/jquery-ui.min.js',
+            'modeltranslation/js/tabbed_translation_fields.js',
+        )
+        css = {
+            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
+        }
