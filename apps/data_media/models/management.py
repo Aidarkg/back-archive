edb64890e44abee5053211b6e001bf5a0680f36a
Nurikov <asankylovnursultan@gmail.com>
2024-05-12 01:18:04 +0600

    Refactor models Management

--- apps/data_media/models/management.py
+++ apps/data_media/models/management.py
@@ -2,6 +2,51 @@ from django.db import models
 from apps.common.models.mixins import DateTimeMixin
 
 
+class ManagementEducation(models.Model):
+    year = models.PositiveSmallIntegerField(
+        blank=False,
+        null=False,
+        verbose_name='Год образования'
+    )
+    place = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+        verbose_name='Место образования'
+    )
+    specialization = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False,
+        verbose_name='Специальность'
+    )
+
+
+class ManagementWork(models.Model):
+    start_year = models.DateField(
+        blank=False,
+        null=False,
+        verbose_name=''
+    )
+    end_year = models.DateField(
+        blank=False,
+        null=False,
+        verbose_name=''
+    )
+    place = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+        verbose_name='Место работы'
+    )
+    position = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+        verbose_name='Должность'
+    )
+
+
 class Management(DateTimeMixin):
     full_name = models.CharField(
         max_length=150,
@@ -27,6 +72,12 @@ class Management(DateTimeMixin):
         default=0,
         verbose_name='Стаж'
     )
+    birth_date = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name='Дата рождения'
+    )
+
 
     def __str__(self) -> str:
         return self.full_name
