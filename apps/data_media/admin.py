ba58e9d16615b0c3a9a03a2961b09dcc955f7a4d
Aidarkg <adijamgyrchiev@gmail.com>
2024-05-15 03:23:54 +0600

    fix admin

--- apps/data_media/admin.py
+++ apps/data_media/admin.py
@@ -1,21 +1,22 @@
 from django.contrib import admin
 from apps.common.admin.mixins import BaseAdminMixin
-from apps.data_media.models import News, PhotoGallery, VideoData, Management, Service, KODEKS, Contact, Visit
+from apps.data_media.models import News, PhotoGallery, VideoData, Management, Service, KODEKS, Contact, Visit, \
+    Organization
 
 
 class VideoDataAdmin(BaseAdminMixin):
     list_display = ['id', 'title', 'description', 'created_at', 'updated_at']
-    fields = ['id', 'video', 'title', 'description', 'created_at', 'updated_at']
+    fields = ['video', 'title', 'description']
 
 
 class PhotoGalleryAdmin(BaseAdminMixin):
     list_display = ['id', 'title', 'description', 'picture', 'created_at', 'updated_at']
-    fields = ['id', 'title', 'description', 'picture', 'created_at', 'updated_at']
+    fields = ['title', 'description', 'picture']
 
 
 class NewsAdmin(BaseAdminMixin):
     list_display = ['id', 'title', 'description', 'detailed_description', 'image', 'created_at', 'updated_at']
-    fields = ['id', 'title', 'description', 'detailed_description', 'image', 'created_at', 'updated_at']
+    fields = ['title', 'description', 'detailed_description', 'image']
 
 
 class ManagementAdmin(BaseAdminMixin):
@@ -23,12 +24,12 @@ class ManagementAdmin(BaseAdminMixin):
     list_display_links = ['id', 'full_name']
     search_fields = ['full_name']
 
-    fields = ['id', 'full_name', 'image', 'position', 'experience', 'created_at', 'updated_at']
+    fields = ['full_name', 'image', 'position', 'experience']
 
 
 class KODEKSAdmin(BaseAdminMixin):
     list_display = ['id', 'title', 'pdf_file', 'created_at', 'updated_at']
-    fields = ['id', 'title', 'pdf_file', 'created_at', 'updated_at']
+    fields = ['title', 'pdf_file']
 
 
 class ContactAdmin(BaseAdminMixin):
@@ -36,14 +37,19 @@ class ContactAdmin(BaseAdminMixin):
                     'updated_at']
     list_display_links = ['id', 'address']
     search_fields = ['address']
-    fields = ['id', 'address', 'phone', 'email', 'work_time', 'reception', 'reading_room', 'created_at', 'updated_at']
+    fields = ['address', 'phone', 'email', 'work_time', 'reception', 'reading_room']
 
 
 class VisitAdmin(BaseAdminMixin):
     list_display = ['id', 'date', 'count']
     list_display_links = ['id', 'date']
     search_fields = ['date']
-    fields = ['id', 'date', 'count']
+    fields = ['date', 'count']
+
+
+class OrganizationAdmin(BaseAdminMixin):
+    list_display = ['id', 'title', 'created_at', 'updated_at']
+    fields = ['title', 'logo']
 
 
 admin.site.register(News, NewsAdmin)
@@ -53,4 +59,5 @@ admin.site.register(Management, ManagementAdmin)
 admin.site.register(Service)
 admin.site.register(KODEKS, KODEKSAdmin)
 admin.site.register(Contact, ContactAdmin)
+admin.site.register(Organization, OrganizationAdmin)
 # admin.site.register(Visit, VisitAdmin)
--- apps/data_media/models/organization.py
+++ apps/data_media/models/organization.py
@@ -5,8 +5,7 @@ from apps.common.models.mixins import DateTimeMixin
 
 class Organization(DateTimeMixin):
     title = models.CharField(max_length=100, verbose_name='название организации')
-    logo = models.ImageField(upload_to='logo',verbose_name='логотип')
-
+    logo = models.ImageField(upload_to='logo', verbose_name='логотип')
 
     def __str__(self):
         return self.title
