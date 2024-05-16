8896b07160a7be8308109f343e10013e071b7568
Nurikov <asankylovnursultan@gmail.com>
2024-05-16 13:22:46 +0600

    add celery in moderator

--- apps/moderator/tasks.py
+++ apps/moderator/tasks.py
@@ -1,7 +1,9 @@
 from django.core.mail import send_mail
 from django.conf import settings
+from celery import shared_task
 
 
+@shared_task
 def send_email_with_credentials(self, password):
         send_mail(
             'Вы были добавлены в модераторы',
