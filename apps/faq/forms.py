21b0ff59c06c9cf729e8857a0f4d6f82795bfa35
Nurikov <asankylovnursultan@gmail.com>
2024-05-16 15:07:22 +0600

    refactor redis

--- apps/faq/forms.py
+++ apps/faq/forms.py
@@ -1,21 +1,24 @@
-from django import forms
-from apps.faq.models import Answer
-from django.conf import settings
-
-
-class AnswerForm(forms.ModelForm):
-    class Meta:
-        model = Answer
-        fields = ['question', 'answer']
-
-    def send_mail_to_user(sender, instance, **kwargs):
-        from apps.faq.tasks import send_mail
-
-        question = instance.question
-        answer = instance.answer
-        send_mail.delay(
-            question.id,
-            from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[question.email],
-            answer=answer
-        )
+# from django import forms
+# from apps.faq.models import Answer
+# from django.conf import settings
+#
+#
+# class AnswerForm(forms.ModelForm):
+#     class Meta:
+#         model = Answer
+#         fields = (
+#             'question',
+#             'answer'
+#         )
+#
+#     def save(self, instance, **kwargs):
+#         from apps.faq.tasks import send_mail
+#
+#         question = instance.question
+#         answer = instance.answer
+#         send_mail.delay(
+#             question.id,
+#             from_email=settings.EMAIL_HOST_USER,
+#             recipient_list=[question.email],
+#             answer=answer
+#         )
