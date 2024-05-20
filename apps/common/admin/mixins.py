90789af4a31a2a9de93613d53c00e1f20d9c771a
Aidarkg <adijamgyrchiev@gmail.com>
2024-05-20 22:13:08 +0600

    Добавил сортировку

--- apps/common/admin/mixins.py
+++ apps/common/admin/mixins.py
@@ -6,6 +6,7 @@ class BaseAdminMixin(TranslationAdmin):
     search_fields = 'title',
     list_filter = ('created_at', 'updated_at')
     list_display_links = ('id', 'title')
+    ordering = ['id', 'created_at', 'updated_at']
 
     readonly_fields = ('id', 'created_at', 'updated_at')
     group_fieldsets = True
