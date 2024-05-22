af2f1af0422f1b39bc444b777e9901bac42d24bf
beknazar <680633@gmail.com>
2024-05-22 23:18:29 +0600

    Написал сжатие изображении.

--- apps/data_media/models/photo_gallery_models.py
+++ apps/data_media/models/photo_gallery_models.py
@@ -1,20 +1,20 @@
 from django.db import models
-
+from apps.data_media.services.photo_compress import WEBPField
 from apps.common.models.mixins import DateTimeMixin
 
 
 class Photo(models.Model):
     gallery = models.ForeignKey('PhotoGallery', on_delete=models.CASCADE, related_name='photo')
-    photo = models.ImageField(upload_to='gallery_photo', verbose_name='Изображение')
+    photo = WEBPField(upload_to='gallery_photo', verbose_name='Изображение')
 
     def __str__(self):
         return f"Photo {self.id}"
 
 
 class PhotoGallery(DateTimeMixin):
-    title = models.CharField(max_length=50,verbose_name='Заголовок')
+    title = models.CharField(max_length=50, verbose_name='Заголовок')
     description = models.TextField(max_length=1000, blank=False, null=False, verbose_name='Описание')
-    picture = models.ImageField(blank=False, null=False, upload_to='gallery/logo', verbose_name='Картинка')
+    picture = WEBPField(blank=False, null=False, upload_to='gallery/logo', verbose_name='Картинка')
 
     def __str__(self):
         return self.title
@@ -22,3 +22,4 @@ class PhotoGallery(DateTimeMixin):
     class Meta:
         verbose_name = 'Фотогалерея'
         verbose_name_plural = 'Фотогалерея'
+
