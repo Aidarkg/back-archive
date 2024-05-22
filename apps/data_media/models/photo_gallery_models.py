bcca4fa8fd744b1a698c93f92ae3006e25457384
pavel <pavelposledov89@gmail.com>
2024-05-22 18:28:14 +0600

    Добавлено возможность добавлять множество фотографий в фотогалереи

--- apps/data_media/models/photo_gallery_models.py
+++ apps/data_media/models/photo_gallery_models.py
@@ -3,10 +3,18 @@ from django.db import models
 from apps.common.models.mixins import DateTimeMixin
 
 
+class Photo(models.Model):
+    gallery = models.ForeignKey('PhotoGallery', on_delete=models.CASCADE, related_name='photo')
+    photo = models.ImageField(upload_to='gallery_photo', verbose_name='Изображение')
+
+    def __str__(self):
+        return f"Photo {self.id}"
+
+
 class PhotoGallery(DateTimeMixin):
-    title = models.CharField(max_length=50, blank=False, null=False, verbose_name='Заголовок')
+    title = models.CharField(max_length=50,verbose_name='Заголовок')
     description = models.TextField(max_length=1000, blank=False, null=False, verbose_name='Описание')
-    picture = models.ImageField(blank=False, null=False, upload_to='gallery', verbose_name='Картинка')
+    picture = models.ImageField(blank=False, null=False, upload_to='gallery/logo', verbose_name='Картинка')
 
     def __str__(self):
         return self.title
--- apps/data_media/serializers/photo_gallery_serializers.py
+++ apps/data_media/serializers/photo_gallery_serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
-from apps.data_media.models import PhotoGallery
+from apps.data_media.models import PhotoGallery, Photo
+
+
+class PhotoListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PhotoGallery
+        fields = ['id', 'title', 'description', 'picture']
+
+
+class PhotoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Photo
+        fields = 'photo'.split()
 
 
 class PhotoGallerySerializer(serializers.ModelSerializer):
+    photo = PhotoSerializer(many=True, read_only=False)
+
     class Meta:
         model = PhotoGallery
-        fields = ('title', 'description', 'picture')
+        fields = ['id', 'title', 'description', 'picture', 'photo']
