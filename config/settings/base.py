f292a9633be7e63e4d166d71834445af81f7cc49
Aidarkg <adijamgyrchiev@gmail.com>
2024-05-24 18:28:01 +0600

    fixed baase

--- config/settings/base.py
+++ config/settings/base.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
+    'PAGE_SIZE': 10,
 
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
@@ -164,5 +164,8 @@ INTERNAL_IPS = [
 ]
 
 CORS_ORIGIN_WHITELIST = [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'http://*:*',
     'http://*',
 ]
