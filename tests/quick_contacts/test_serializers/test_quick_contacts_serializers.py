5ccfe0928b0a780f854a409a92f29f694da08875
beknazar <680633@gmail.com>
2024-05-26 16:05:32 +0600

    Исправил поле expected_fields

--- tests/quick_contacts/test_serializers/test_quick_contacts_serializers.py
+++ tests/quick_contacts/test_serializers/test_quick_contacts_serializers.py
@@ -18,6 +18,7 @@ class QuickContactSerializerTestCase(TestCase):
 
     def test_serializer_fields(self):
         expected_fields = {
+            'id',
             'address',
             'phone',
             'email',
