a14fdc01b3d2b0ba00081975a17ce1fdd323685f
Aidarkg <adijamgyrchiev@gmail.com>
2024-06-06 12:28:14 +0600

    fixed merge

--- apps/information/models/organization.py
+++ apps/information/models/organization.py
@@ -1,13 +1,11 @@
 from django.db import models
-from apps.information.services.photo_compress import WEBPField
 from apps.common.models.mixins import DateTimeMixin
+from apps.information.services.photo_compress import WEBPField
 
 
 class Organization(DateTimeMixin):
     title = models.CharField(max_length=300, verbose_name='Название организации')
-    logo = models.ImageField(upload_to='logo', verbose_name='Логотип')
-    title = models.TextField(verbose_name='название организации')
-    logo = WEBPField(upload_to='logo', verbose_name='логотип')
+    logo = WEBPField(upload_to='logo', verbose_name='Логотип')
 
     def __str__(self):
         return self.title
--- apps/information/models/video_data.py
+++ apps/information/models/video_data.py
@@ -1,5 +1,4 @@
 from django.db import models
-from apps.information.services.photo_compress import WEBPField
 from apps.common.models.mixins import DateTimeMixin
 from apps.information.services.video_data import validate_link
 
@@ -22,7 +21,7 @@ class VideoLink(DateTimeMixin):
     title = models.CharField(max_length=500, verbose_name='Название')
     video_link = models.CharField(max_length=300, validators=[validate_link], verbose_name='Ссылка на видео')
     public_date = models.DateTimeField(verbose_name='Дата публикации')
-    cover = WEBPField(
+    cover = models.ImageField(
         upload_to='video/cover',
         null=True,
         blank=True,
