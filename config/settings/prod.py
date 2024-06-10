99d948a7c2c2d0542539c3efdc6c4a645f5f10d7
Aidarkg <adijamgyrchiev@gmail.com>
2024-06-11 02:39:56 +0600

    docker

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,10 +1,6 @@
 import os
 from decouple import config as env
 
-from config.settings.base import BASE_DIR
-
-SECRET_KEY = env('SECRET_KEY')
-
 # Database
 DATABASES = {
     'default': {
@@ -16,12 +12,3 @@ DATABASES = {
         'PORT': env('POSTGRES_PORT'),
     }
 }
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'http')
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
-SECURE_HSTS_SECONDS = 3600
