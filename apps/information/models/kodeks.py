34843b55a8b5be31b8c65526c266322b36727bd9
Aidarkg <adijamgyrchiev@gmail.com>
2024-06-16 18:19:54 +0600

    Нимного прошелся по коду

--- apps/information/models/kodeks.py
+++ apps/information/models/kodeks.py
@@ -4,8 +4,16 @@ from apps.information.services.validator import validate_pdf_format
 
 
 class KODEKS(DateTimeMixin):
-    title = models.CharField(max_length=500, verbose_name='Название')
-    pdf_file = models.FileField(upload_to='pdf_files', validators=[validate_pdf_format], verbose_name='Файл')
+    title = models.CharField(
+        max_length=500,
+        verbose_name='Название'
+    )
+    pdf_file = models.FileField(
+        upload_to='pdf_files',
+        validators=[validate_pdf_format],
+        help_text='Загрузите только PDF файл',
+        verbose_name='Файл'
+    )
     date_file = models.DateField(verbose_name='Дата')
     document_number = models.PositiveIntegerField(verbose_name='Номер документа')
 
