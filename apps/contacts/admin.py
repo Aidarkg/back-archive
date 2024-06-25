e1cf3c3e4150c8722413c78cbc4fcdfadb6d741e
Aidarkg <adijamgyrchiev@gmail.com>
2024-06-25 20:23:59 +0600

    modif admin contact

--- apps/contacts/admin.py
+++ apps/contacts/admin.py
@@ -9,17 +9,26 @@ class ReceptionAdmin(TranslationStackedInline):
     extra = 1
     verbose_name_plural = 'Приём граждан'
 
+    def get_max_num(self, request, obj=None, **kwargs):
+        return 1
+
 
 class ReadingRoomAdmin(TranslationStackedInline):
     model = ReadingRoom
     extra = 1
     verbose_name_plural = 'Читальный зал'
 
+    def get_max_num(self, request, obj=None, **kwargs):
+        return 1
+
 
 class ScheduleAdmin(TranslationStackedInline):
     model = Schedule
     extra = 1
     verbose_name_plural = 'График работы'
+
+    def get_max_num(self, request, obj=None, **kwargs):
+        return 1
 
 
 class ArchiveContactAdmin(BaseAdminMixin):
@@ -30,6 +39,12 @@ class ArchiveContactAdmin(BaseAdminMixin):
 
     inlines = [ReceptionAdmin, ReadingRoomAdmin, ScheduleAdmin]
 
+    def has_add_permission(self, request):
+        if ArchiveContact.objects.all().count() == 1:
+            return False
+        else:
+            return True
+
 
 class CollCenterAdmin(admin.ModelAdmin):
     list_display = ('number',)
@@ -38,12 +53,24 @@ class CollCenterAdmin(admin.ModelAdmin):
     fields = ('number',)
     list_filter = ('created_at', 'updated_at')
 
+    def has_add_permission(self, request):
+        if CollCenter.objects.all().count() == 1:
+            return False
+        else:
+            return True
+
 
 class AnticoreAdmin(BaseAdminMixin):
     list_display = ('location', 'phone_number', 'email')
     fields = ('location', 'phone_number', 'email')
     list_display_links = ('location', 'phone_number', 'email')
     search_fields = ('location',)
+
+    def has_add_permission(self, request):
+        if Anticore.objects.all().count() == 1:
+            return False
+        else:
+            return True
 
 
 class ContactAdmin(BaseAdminMixin):
